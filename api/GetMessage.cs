81be0474a9b781a4d1363cd4bc04900bd4487df8
Simon Moreau <simon@bim42.com>
2020-11-07 15:31:41 +0100

    remove workflows

--- api/GetMessage.cs
+++ api/GetMessage.cs
@@ -29,7 +29,7 @@ namespace Vanilla.Function
             name = name ?? data?.name;
 
             string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                ? "This HTTP triggered function executed successfully this time again. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully this time again.";
 
             object responseObject = new
