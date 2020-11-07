a8c8db7059c0fc2d79aed3ab5a5b705cdaabbb27
Simon Moreau <simon@bim42.com>
2020-11-07 15:27:03 +0100

    add github deployment for the Functions

--- api/GetMessage.cs
+++ api/GetMessage.cs
@@ -30,7 +30,7 @@ namespace Vanilla.Function
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                : $"Hello, {name}. This HTTP triggered function executed successfully this time again.";
 
             object responseObject = new
             {
